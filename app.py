6afb27f4c736f6d95d5e4dad6cb91127fb4750e0
cadoCG <77072784+cadoCG@users.noreply.github.com>
2025-05-25 20:49:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, render_template
 from flask_cors import CORS
 from rembg import remove, new_session
 from PIL import Image
